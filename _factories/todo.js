187a55f6f21ef53fd83880f53ad70900a366e137
Jeff Cross <middlefloow@gmail.com>
2013-10-15 12:21:22 -0700

    style(Todo): Reduced line sizes in Todo factory

--- _factories/todo.js
+++ _factories/todo.js
@@ -1,8 +1,9 @@
 angular.module('todos')
-  .factory('Todo', ['$resource', 'TODOS_PATH', function ($resource, TODOS_PATH) {
-    return $resource(TODOS_PATH, null, {
-      update: {
-        url: TODOS_PATH + '/:id',
-        method: 'PUT'
-      }});
-  }]);
+  .factory('Todo', ['$resource', 'TODOS_PATH',
+    function ($resource, TODOS_PATH) {
+      return $resource(TODOS_PATH, null, {
+        update: {
+          url: TODOS_PATH + '/:id',
+          method: 'PUT'
+        }});
+    }]);
