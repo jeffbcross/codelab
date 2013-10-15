0187c19ebe2e50b6426ec44c20f92d3ce1d988bc
Jeff Cross <middlefloow@gmail.com>
2013-10-15 12:22:29 -0700

    style(createTodo): Reduced line sizes in CreateTodoController

--- _directives/CreateTodoController.js
+++ _directives/CreateTodoController.js
@@ -1,12 +1,13 @@
 angular.module('todos')
-  .controller('CreateTodoController', ['$scope', 'tdTodosStore', function ($scope, tdTodosStore) {
-    $scope.saveTodo = function () {
-      tdTodosStore.add({
-        text: $scope.newTodo.text,
-        done: $scope.newTodo.done || false
-      });
+  .controller('CreateTodoController', ['$scope', 'tdTodosStore',
+    function ($scope, tdTodosStore) {
+      $scope.saveTodo = function () {
+        tdTodosStore.add({
+          text: $scope.newTodo.text,
+          done: $scope.newTodo.done || false
+        });
 
-      //Reset the model
-      angular.extend($scope.newTodo, {text: null, done: null, id: null});
-    };
-  }]);
+        //Reset the model
+        angular.extend($scope.newTodo, {text: null, done: null, id: null});
+      };
+    }]);
