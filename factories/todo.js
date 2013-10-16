aed206bc732196f9106ea60bbe06a14d8878fa9c
Jeff Cross <middlefloow@gmail.com>
2013-10-16 11:03:41 -0700

    fix(Todo): Removed temporary Get action

--- factories/todo.js
+++ factories/todo.js
@@ -2,14 +2,6 @@ angular.module('todos')
   .factory('Todo', ['$resource', 'TODOS_PATH',
     function ($resource, TODOS_PATH) {
       return $resource(TODOS_PATH, null, {
-        //TODO: Remove this action when the service is complete
-        get: {
-          method: 'GET',
-          url: TODOS_PATH,
-          transformResponse: function (todos) {
-            return JSON.parse(todos)[0];
-          }
-        },
         update: {
           url: TODOS_PATH + '/:id',
           method: 'PUT'
