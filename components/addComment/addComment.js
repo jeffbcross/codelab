12933858f50c7430130be7d58d0de58f1a3f645f
Jeff Cross <middlefloow@gmail.com>
2013-10-16 21:51:02 -0700

    style(addComment): Removed  namespace from test description

--- components/addComment/addComment.js
+++ components/addComment/addComment.js
@@ -1,5 +1,5 @@
 angular.module('todos')
-  .directive('tdAddComment', ['$parse', function ($parse) {
+  .directive('addComment', ['$parse', function ($parse) {
     return {
       restrict: 'E',
       templateUrl: 'components/addComment/add-comment.html',
