17996cb8d4d5c20984ba367f034e23eae22c3be6
Jeff Cross <middlefloow@gmail.com>
2013-10-16 21:49:31 -0700

    style(todoComments): Reduced length of definition line

--- components/todoComments/todoComments.js
+++ components/todoComments/todoComments.js
@@ -1,12 +1,13 @@
 angular.module('todos')
-  .directive('tdTodoComments', ['$parse', 'tdTodoCommentsStore', function ($parse, tdTodoCommentsStore) {
-    return {
-      restrict: 'E',
-      scope: true,
-      templateUrl: 'components/todoComments/todo-comments.html',
-      link: function (scope, element, attrs) {
-        var todoId = $parse(attrs.todoId)(scope);
-        scope.comments = tdTodoCommentsStore.getCommentsForTodo(todoId);
+  .directive('tdTodoComments', ['$parse', 'tdTodoCommentsStore',
+    function ($parse, tdTodoCommentsStore) {
+      return {
+        restrict: 'E',
+        scope: true,
+        templateUrl: 'components/todoComments/todo-comments.html',
+        link: function (scope, element, attrs) {
+          var todoId = $parse(attrs.todoId)(scope);
+          scope.comments = tdTodoCommentsStore.getCommentsForTodo(todoId);
+        }
       }
-    }
-  }]);
+    }]);
