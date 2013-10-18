342e7bdb2946cf80975d7f6c787cf8a7a40c1207
Jeff Cross <middlefloow@gmail.com>
2013-10-18 09:52:56 -0700

    refactor(TodoDetail): Moved todoComments into todoDetail view

--- todo_detail/TodoDetailController.js
+++ todo_detail/TodoDetailController.js
@@ -2,11 +2,11 @@ angular.module('todos')
   .controller('TodoDetailController',
     ['$scope', '$routeParams', 'tdTodosStore', 'tdTodoCommentsStore', 'Todo',
       function ($scope, $routeParams, tdTodosStore, tdTodoCommentsStore, Todo) {
-        this.todoId = $routeParams.id;
         this.todo = Todo.get({id: $routeParams.id});
+        this.comments = tdTodoCommentsStore.getCommentsForTodo($routeParams.id);
 
         this.updateHeading = function (todo) {
-          tdTodosStore.updateById(todo.id, todo);
+          tdTodosStore.updateById($routeParams.id, todo);
           this.editingText = false;
         };
 
@@ -17,7 +17,7 @@ angular.module('todos')
 
         $scope.$watch('todo.done', function (newVal, oldVal) {
           if (typeof oldVal === 'boolean' && newVal !== oldVal) {
-            tdTodosStore.updateById(this.todoId, this.todo);
+            tdTodosStore.updateById($routeParams.id, this.todo);
           }
         });
     }]);
