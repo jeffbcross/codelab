3a6bab25ef92d34b8910c4475d75b3f2a7962a46
Jeff Cross <middlefloow@gmail.com>
2013-10-18 15:12:56 -0700

    feat(Creator): Added creation metadata to terms

--- services/profileStore.js
+++ services/profileStore.js
@@ -5,18 +5,20 @@ angular.module('todoApp')
       return 'http://gravatar.com/avatar/' + emailHash;
     }
   })
-  .service('tdProfileStore', ['$window', 'tdGravatarEmail', 'LOCALSTORAGE_PROFILE_KEY', function ($window, tdGravatarEmail, LOCALSTORAGE_PROFILE_KEY) {
-    var user = $window.localStorage.getItem(LOCALSTORAGE_PROFILE_KEY);
-    user = JSON.parse(user || '{}');
-    this.picture = user.picture;
-    this.email = user.email;
+  .service('tdProfileStore',
+    ['$window', 'tdGravatarEmail', 'LOCALSTORAGE_PROFILE_KEY',
+    function ($window, tdGravatarEmail, LOCALSTORAGE_PROFILE_KEY) {
+      var user = $window.localStorage.getItem(LOCALSTORAGE_PROFILE_KEY);
+      user = JSON.parse(user || '{}');
+      this.picture = user.picture;
+      this.email = user.email;
 
-    this.save = function () {
-      this.picture = tdGravatarEmail(this.email);
+      this.save = function () {
+        this.picture = tdGravatarEmail(this.email);
 
-      $window.localStorage.setItem(LOCALSTORAGE_PROFILE_KEY, JSON.stringify({
-        email: this.email,
-        picture: this.picture
-      }));
-    };
-  }])
+        $window.localStorage.setItem(LOCALSTORAGE_PROFILE_KEY, JSON.stringify({
+          email: this.email,
+          picture: this.picture
+        }));
+      };
+    }])
--- terms/TermsListController.js
+++ terms/TermsListController.js
@@ -1,8 +1,10 @@
 angular.module('todoApp')
   .controller('TermsListController',
-    ['$scope', 'tdTermsStore',
-    function ($scope, tdTermsStore) {
+    ['$scope', 'tdTermsStore', 'tdProfileStore',
+    function ($scope, tdTermsStore, tdProfileStore) {
       this.terms = tdTermsStore.todos;
+      this.currentUser = tdProfileStore;
+      console.log('currentUser', this.currentUser)
 
       this.saveTerm = function () {
         tdTermsStore.add({
